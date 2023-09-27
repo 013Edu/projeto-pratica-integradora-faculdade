a869a9ce6c435c9b8bf8d055cecb9b7271228f5f
Eduardo Gonçalves <91925011+013Edu@users.noreply.github.com>
2023-09-27 19:50:35 -0300

    correcao de erros de tipagens, mudei a forma como estavam na funcao createData, pois estavam usando tipagem, porém o código esta em javascrip e fiz alteracao na cor do botao criar tarefa

---
+++ src/theme.jsx
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976D2',
+        },
+        secondary: {
+            main: '#FF5722',
+        },
+        customColor: {
+            main: '#00FF00',
+        },
+    },
+});
+
+export default theme;
